7295d622eef47de0544b210fcf388aa10573f675
Adam Schiller <ahs8w@virginia.edu>
2020-03-13 21:10:45 -0700

    increase swap

--- .ebextensions/setup_swap.sh
+++ .ebextensions/setup_swap.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 SWAPFILE=/var/swapfile
-SWAP_MEGABYTES=2048
+SWAP_MEGABYTES=4096
 
 if [ -f $SWAPFILE ]; then
 	echo "Swapfile $SWAPFILE found, assuming already setup"
 	exit;
 fi
 
-/bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_MEGABYTES
+/bin/dd if=/dev/zero of=$SWAPFILE bs=128M count=32
 /bin/chmod 600 $SWAPFILE
 /sbin/mkswap $SWAPFILE
 /sbin/swapon $SWAPFILE
+echo /sbin/swapon -s
